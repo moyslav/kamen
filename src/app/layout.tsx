37ae0774a30cd4ae2ddc09957a02e72a80ad6d57
moyslav <moyslav@github.com>
2026-06-23 19:04:15 +0500

    fix: move Google Fonts import from CSS to HTML link tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,6 +40,9 @@ export default function RootLayout({
       className="h-full antialiased"
     >
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Cormorant:wght@300;400;500;600;700&family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
         <script
           dangerouslySetInnerHTML={{
             __html: `
